0aa11849b9e4c550cb8292fdde85fd18d4e2c34a
Alef Rodrigues <alef@widelab.com.br>
2025-06-24 17:29:46 -0300

    fix/adjust config to netlify deploy

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -8,7 +8,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Importa a instância do seu aplicativo Express do src/app.ts
-import { App as app } from '../../app'; // Ajuste o caminho se necessário
+import { App as app } from '../../src/app'; // Ajuste o caminho se necessário
 
 // Crie o handler serverless a partir do seu aplicativo Express
 // Isso "envelopa" seu app Express para ser executado como uma função Lambda
