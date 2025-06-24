a3911dfa7971ca6160667e65e6f22981ce45ac69
Alef Rodrigues <alef@widelab.com.br>
2025-06-24 17:44:29 -0300

    fix/adjust config to netlify deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,14 @@ module.exports = {
   apps: [
     {
       name: "coupon-server", // Nome da sua aplicação
-      script: "./dist/server.js", // Caminho para o arquivo JS compilado (pós-build)
+      script: "./dist/src/server.js", // Caminho para o arquivo JS compilado (pós-build)
       instances: "max", // Inicia o máximo de instâncias possível com base nas CPUs
       exec_mode: "cluster", // Ativa o modo cluster do Node.js (load balancing)
       watch: false, // Não monitorar arquivos em produção (use apenas em dev)
       max_memory_restart: "1G", // Reinicia se o uso de memória exceder 1GB
       env: {
         NODE_ENV: "production", // Garante que o ambiente seja de produção
-        PORT: 3000, // Porta da sua aplicação, pode ser sobrescrita por variáveis de ambiente do sistema
+        PORT: 3001, // Porta da sua aplicação, pode ser sobrescrita por variáveis de ambiente do sistema
       },
       env_production: {
         NODE_ENV: "production",
